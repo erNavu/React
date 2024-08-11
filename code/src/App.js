582a82ae8df953ddf06cb48272033b970ae7bb61
navpreet boparai <kaurnavpreet50@gmail.com>
2024-08-11 22:28:52 +0530

    Class 8

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Outlet, Route, createRoutesFromElements } from 'react-router-dom'
 
 import About from './pages/About';
 import ContactUs from "./pages/ContactUs";
@@ -12,6 +12,14 @@ import Login from "./pages/Login";
 import RestaurantMenu from "./components/RestaurantMenu";
 
 
+// const User = () => {
+//     return <h1>Hi I am User page</h1>
+// }
+
+// const AboutLayout = () => {
+//     return <Outlet />
+// }
+
 const App = () => {
     return (<div className="app">
         <Header />
@@ -19,6 +27,8 @@ const App = () => {
         <Footer />
     </div>)
 }
+
+// appRouter is same as jsxRouter . only syntax difference
 
 const appRouter = createBrowserRouter([
     {
@@ -34,9 +44,23 @@ const appRouter = createBrowserRouter([
                 path: "/login",
                 element: <Login />
             },
+            // {
+            //     path: "/about",
+            //     element: <AboutLayout />,
+            //     children: [
+            //         {
+            //             path: "/about",
+            //             element: <About />
+            //         },
+            //         {
+            //             path: "/about/user",
+            //             element: <User />
+            //         }
+            //     ]
+            // },
             {
                 path: "/about",
-                element: <About />
+                element: <About />,
             },
             {
                 path: "/contact",
@@ -49,6 +73,22 @@ const appRouter = createBrowserRouter([
         ]
     }
 ])
+
+// const jsxRouter = createBrowserRouter(
+//     createRoutesFromElements(
+//         <Route path="/" element={<App />}>
+//             <Route path="/" element={<Home />} />
+//             <Route path="contact" element={<ContactUs />} />
+//             <Route path="about" element={<AboutLayout />} >
+//             <Route path="" element={<About />} />
+//                 <Route path="user" element={<User />}></Route>
+//             </Route>
+//             <Route path="login" element={<Login />} />
+//             <Route path="restaurant/:res" element={<RestaurantMenu />} />
+
+//         </Route>
+//     )
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={appRouter} />);
